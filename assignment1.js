a012009bfb4a39824908c18d3de591fc292c18b3
sanchitamk <shasanch@sheridancollege.ca>
2023-09-26 15:29:24 -0400

    Part 1
    Create a data structure that describes a "hospital". It has a name and contains 3 "patient" records. Each "patient" has an id, fullName, dateOfBirth, and multiple "symptoms". Each symptom in a String.

---
+++ assignment1.js
@@ -0,0 +1,45 @@
+//Part1
+let hospital = {
+   
+    name : "Max Hospital",
+    patients :[
+ 
+       { 
+         
+       PatientId : "1", 
+       PatientName:"Mark Kay",
+       PatientDOB:"12 September 1990",
+       Symptoms: ["RunnyNose", "Headache", "Fever"]
+       },
+
+
+       { 
+         
+        PatientId : "2", 
+        PatientName:"Grace Goldman",
+        PatientDOB:"4 June 2000",
+        Symptoms:["Sore Throat", "Bodyache", "Fatigue"]
+        },
+
+
+        { 
+         
+            PatientId : "3", 
+            PatientName:"Jay Hart",
+            PatientDOB:"23 Februrary 2005",
+            Symptoms:["RunnyNose", "Headache", "Fever"]
+            }
+ 
+     ]
+ 
+    };
+
+    
+
+
+
+    
+
+
+
+
