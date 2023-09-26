40b63261583041dc30763b8fb9b2e329a14f9812
sanchitamk <shasanch@sheridancollege.ca>
2023-09-26 15:31:30 -0400

    Part 2
    Create a function named "showPatients" that receives a hospital data structure and returns the following as a string:
    
    <h1>Hospital Name</h1>
    <h2>Patient full name, date of birth </h2>
    <ul>
      <li>symptom</li>
      <li>symptom</li>
      <li>symptom</li>
    </ul>
    Repeat the h2, ul, and li for each patient.
    
    Execute that function with the data structure created in Part 1 then console.log the return value to the document.

--- assignment1.js
+++ assignment1.js
@@ -34,7 +34,28 @@ let hospital = {
  
     };
 
-    
+    //Part 2
+let showPatients= function (hospital){
+    let hospitaldata = "<h1>"+ hospital.name + "</h1>\n\n";
+            
+    for (let i =0; i<hospital.patients.length; i++){
+
+        hospitaldata += "<h2>"+ hospital.patients[i].PatientName + ", " +  hospital.patients[i].PatientDOB + "</h2>\n";
+        hospitaldata += "<ul>\n"
+
+        for (let s =0; s<hospital.patients[i].Symptoms.length; s++){
+
+            hospitaldata += "<li>"+ hospital.patients[i].Symptoms[s] + "</li>\n";
+        }
+        hospitaldata += "</ul>\n\n"
+    }
+
+    return hospitaldata 
+}
+
+let patientInfo = showPatients(hospital);
+console.log(patientInfo);
+
 
 
 
